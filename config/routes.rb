9808b7a3f5feeff79189558a4023c9cd3cdb53e1
Aditi Parihar <aditiparihar06@gmail.com>
2021-09-17 17:57:33 +0530

    add author column to articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  resources :articles
+  resources :articles # this will default create all 7 routes 
+  get '/all_articles' => "articles#index" # creating particular route
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_16_080626) do
+ActiveRecord::Schema.define(version: 2021_09_17_121855) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "author"
   end
 
 end
